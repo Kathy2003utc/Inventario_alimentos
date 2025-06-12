2186c8a9188589141a3deb781dfc6745c963b469
Katherin <katherin.pico2124@utc.edu.ec>
2025-06-11 23:42:17 -0500

    Migracion de alimentos

--- Aplicaciones/Alimentos/models.py
+++ Aplicaciones/Alimentos/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Alimento(models.Model):
+    nombre = models.CharField(max_length=100)
+    tipo = models.CharField(max_length=50)
+    foto_alimento = models.ImageField(upload_to='Alimentos', null=True, blank=True)
+
