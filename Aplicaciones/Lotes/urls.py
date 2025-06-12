7ef5fa6d08b6b8d4d198897db8912e8096c48d3f
Katherin <katherin.pico2124@utc.edu.ec>
2025-06-12 09:44:04 -0500

    urls de lotes

---
+++ Aplicaciones/Lotes/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('listaLotes/', views.listaLotes, name='listaLotes'),
+    path('nuevoLote/', views.nuevoLote, name='nuevoLote'),
+    path('guardarLote/', views.guardarLote, name='guardarLote'),
+    path('eliminarLote/<int:id>/', views.eliminarLote, name='eliminarLote'),
+    path('editarLote/<int:id>/', views.editarLote, name='editarLote'),
+    path('procesarEdicionLote/', views.procesarEdicionLote, name='procesarEdicionLote'),
+]
