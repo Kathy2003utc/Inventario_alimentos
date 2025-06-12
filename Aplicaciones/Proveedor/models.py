91a2b6a3b499995e0487e47dcf52e69cf98149ff
Katherin <katherin.pico2124@utc.edu.ec>
2025-06-12 00:31:36 -0500

    migracion de proveedor

--- Aplicaciones/Proveedor/models.py
+++ Aplicaciones/Proveedor/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Proveedor(models.Model):
+    nombre = models.CharField(max_length=100)
+    telefono = models.CharField(max_length=20)
+    pdf_certificado_sanitario = models.FileField(upload_to='proveedores/certificados/', null=True, blank=True)
+
+    def __str__(self):
+        return self.nombre
